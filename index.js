877279c542db7fe65775ce680fbe9e11cae6b5c0
SukiSugi <166683785+SukiSugi@users.noreply.github.com>
2024-04-16 07:13:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,94 +1,245 @@
-function func_one() {
-  let one = document.getElementById("number_one").value;
-  document.getElementById("input-screen").value = one;
+let numberZero = document.getElementById("number_zero");
+let numberOne = document.getElementById("number_one");
+let numberTwo = document.getElementById("number_two");
+let numberThree = document.getElementById("number_three");
+let numberFour = document.getElementById("number_four");
+let numberFive = document.getElementById("number_five");
+let numberSix = document.getElementById("number_six");
+let numberSeven = document.getElementById("number_seven");
+let numberEight = document.getElementById("number_eight");
+let numberNine = document.getElementById("number_nine");
+
+let numberDot = document.getElementById("number_dot");
+let numberAC = document.getElementById("number_ac");
+let numberAdd = document.getElementById("number_add");
+let numberMinus = document.getElementById("number_minus");
+let numberMultiply = document.getElementById("number_multiply");
+let numberDivision = document.getElementById("number_division");
+let numberEquals = document.getElementById("number_equals");
+
+let numberBackspace = document.getElementById("number_backspace");
+
+let inputScreen = document.getElementById("input-screen");
+let inputScreenTop = document.getElementById("input-screen-top");
+let inputPercent = document.getElementById("number_percent");
+
+
+
+
+
+numberZero.value = 0;
+numberOne.value = 1;
+numberTwo.value = 2;
+numberThree.value = 3;
+numberFour.value = 4;
+numberFive.value = 5;
+numberSix.value = 6;
+numberSeven.value = 7;
+numberEight.value = 8;
+numberNine.value = 9;
+
+numberDot.value = ".";
+numberAC.value = "";
+numberAdd.value = " + ";
+numberMinus.value = " - ";
+numberMultiply.value = " * ";
+numberDivision.value = " / ";
+
+inputScreen.value = ""
+
+// inputScreen.value = "";
+
+
+
+
+numberZero.onclick = function(){functionZero()};
+numberOne.onclick = function(){functionOne()};
+numberTwo.onclick = function(){functionTwo()};
+numberThree.onclick = function(){functionThree()};
+numberFour.onclick = function(){functionFour()};
+numberFive.onclick = function(){functionFive()};
+numberSix.onclick = function(){functionSix()};
+numberSeven.onclick = function(){functionSeven()};
+numberEight.onclick = function(){functionEight()};
+numberNine.onclick = function(){functionNine()};
+
+numberDot.onclick = function(){functionDot()};
+numberAC.onclick = function(){functionAC()};
+numberAdd.onclick = function(){functionAdd()};
+numberMinus.onclick = function(){functionMinus()};
+numberMultiply.onclick = function(){functionMultiply()};
+numberDivision.onclick = function(){functionDivision()};
+numberEquals.onclick = function(){functionEquals()};
+
+numberBackspace.onclick = function(){functionBackspace()};
+
+numberEquals.onclick = function(){onclickEqual()};
+
+inputPercent.onclick = function(){functionPercent()};
+
+
+// function functionZero() {
+//   let input = numberZero.value;
+//   // if(inputScreen.value != 0){
+//   //   inputScreen.value += input;
+//   // }
+  
+// }
+
+// function functionOne(){
+//   let input = numberOne.value;
+//   inputScreen.value = input;
+//   inputScreenTop.value += input;
+// }
+
+// function functionTwo(){
+//   let input = numberTwo.value;
+//   inputScreen.value = input;
+//   inputScreenTop.value += input;
+// }
+
+function functionZero() {
+  let input = numberZero.value;
+
+  if(inputScreen.value != 0) {
+    // inputScreen.value = "";
+    inputScreen.value += input;
+  }
+  
 }
 
-function func_two() {
-  let two = document.getElementById("number_two").value;
-  document.getElementById("input-screen").value = two;
+function functionOne(){
+  let input = numberOne.value;
+  inputScreen.value += input;
+
+  // if(inputScreen.value != result) {
+  //   inputScreen.value = "";
+  // }
+
+  
 }
 
-function func_three() {
-  let three = document.getElementById("number_three").value;
-  document.getElementById("input-screen").value = three;
+
+function functionTwo(){
+  let input = numberTwo.value;
+  inputScreen.value += input;
 }
 
-function func_four() {
-  let four = document.getElementById("number_four").value;
-  document.getElementById("input-screen").value = four;
+function functionThree(){
+  let input = numberThree.value;
+  inputScreen.value += input;
 }
 
-function func_five() {
-  let five = document.getElementById("number_five").value;
-  document.getElementById("input-screen").value = five;
+function functionFour(){
+  let input = numberFour.value;
+  inputScreen.value += input;
 }
 
-function func_six() {
-  let six = document.getElementById("number_six").value;
-  document.getElementById("input-screen").value = six;
+function functionFive(){
+  let input = numberFive.value;
+  inputScreen.value += input;
 }
 
-function func_seven() {
-  let seven = document.getElementById("number_seven").value;
-  document.getElementById("input-screen").value = seven;
+function functionSix(){
+  let input = numberSix.value;
+  inputScreen.value += input;
 }
 
-function func_eight() {
-  let eight = document.getElementById("number_eight").value;
-  document.getElementById("input-screen").value = eight;
+function functionSeven(){
+  let input = numberSeven.value;
+  inputScreen.value += input;
 }
 
-function func_nine() {
-  let nine = document.getElementById("number_nine").value;
-  document.getElementById("input-screen").value = nine;
+function functionEight(){
+  let input = numberEight.value;
+  inputScreen.value += input;
 }
 
-function func_zero() {
-  let zero = document.getElementById("number_one").value;
-  document.getElementById("input-screen").value = zero;
+function functionNine(){
+  let input = numberNine.value;
+  inputScreen.value += input;
 }
 
-function func_multiply() {
-  let multiply = document.getElementById("number_multiply").value;
-  document.getElementById("input-screen").value = multiply;
+
+
+
+
+function functionDot(){
+  let input = numberDot.value;
+  inputScreen.value += input;
 }
 
-function func_division() {
-  let division = document.getElementById("number_division").value;
-  document.getElementById("input-screen").value = division;
+function functionAC(){
+  inputScreen.value = "";
+  inputScreenTop.value = " ";
 }
 
-function func_equals() {
-  let equals = document.getElementById("number_equals").value;
-  document.getElementById("input-screen").value = equals;
+function functionAdd(){
+  let input = numberAdd.value;
+  inputScreenTop.value = inputScreen.value;
+  inputScreenTop.value += input;
 }
 
-function func_dot() {
-  let dot = document.getElementById("number_dot").value;
-  document.getElementById("input-screen").value = dot;
+function functionMinus(){
+  let input = numberMinus.value;
+  inputScreenTop.value = inputScreen.value;
+  inputScreenTop.value += input;
 }
 
-function func_minus() {
-  let minus = document.getElementById("number_minus").value;
-  document.getElementById("input-screen").value = minus;
+function functionMultiply(){
+  let input = numberMultiply.value;
+  inputScreenTop.value = inputScreen.value;
+  inputScreenTop.value += input;
 }
 
-function func_percent() {
-  let percent = document.getElementById("number_percent").value;
-  document.getElementById("input-screen").value = percent;
+function functionDivision(){
+  let input = numberDivision.value;
+  inputScreenTop.value = inputScreen.value;
+  inputScreenTop.value += input;
 }
 
-function func_add() {
-  let add = document.getElementById("number_add").value;
-  document.getElementById("input-screen").value = add;
+function onclickEqual() {
+  console.log("Equal Clicked!");
+  inputScreenTop.value += inputScreen.value;
+  inputScreen.value = eval(inputScreenTop.value);
 }
 
-function func_one() {
-  let one = document.getElementById("number_one").value;
-  document.getElementById("input-screen").value = one;
+// function functionPercent() {
+//   inputScreen.value - inputScreen.value*2;
+// }
+
+// function functionEquals(){
+//   let result = eval(inputScreen.value);
+//   inputScreen.value = result;
+// }
+
+
+
+// if (inputScreen.value >= 9 && inputScreen.value <= 0) {
+//   numberEquals.onclick.value = "1";
+// }
+
+// if(inputScreen.value > 9 && inputScreen.value <0)
+//   let inputScreen.value = input;
+
+
+
+
+function functionBackspace(){
+  inputScreen.value = "";
 }
 
-function func_one() {
-  let one = document.getElementById("number_one").value;
-  document.getElementById("input-screen").value = one;
-}
+
+
+
+
+// B  I  G      B  R  E  A  K
+
+
+
+
+
+
+
+ 
+ 
